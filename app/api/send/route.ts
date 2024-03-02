a001e45c037a152a60e0796621edbfb72b28d982
Michal Sadurski <michal.sadurski@bluesoft.com>
2024-03-02 15:11:30 +0100

    route parameter fix

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,10 +1,10 @@
 import { Resend } from 'resend';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import ContactUserEmail from '@/app/emails/welcome';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: NextResponse) {
+export async function POST(req: NextRequest) {
     const data = await req.json()
     const { username, email, phone, message } = data;
 
