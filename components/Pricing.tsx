4c47b93b986e9ba6d8e31bb555c79d860c68270c
Michal Sadurski <michal.sadurski@bluesoft.com>
2024-06-12 01:21:13 +0200

    pricing additional info fix

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -60,7 +60,7 @@ const premiumItemsData = [
 const PricingItem = ({ positive = true, text }: { positive?: boolean, text: string }) =>
     <div className="flex items-center">
         <svg xmlns="http://www.w3.org/2000/svg" className={`${positive ? 'text-blue-500': 'text-red-400'} w-5 h-5`} viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
         </svg>
 
         <span className="mx-4 text-gray-300 w-fit">{text}</span>
@@ -165,12 +165,10 @@ const Pricing = () => {
                                 <span className='font-semibold'>Opcje dodatkowe:</span>
                                 <span>Wywiad z gośćmi, życzenia na koniec filmu</span>
                                 <span>Zatrudnienie drugiego operatora</span>
-                                <span>Wywiad z gośćmi, życzenia na koniec filmu</span>
                             </div>
                             <div className='flex flex-col justify-end leading-relaxed'>
                                 <span>300 PLN</span>
                                 <span>1.500 PLN</span>
-                                <span>300 PLN</span>
                             </div>
                         </div>
                     </div>
